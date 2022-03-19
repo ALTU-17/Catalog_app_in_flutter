7941d76172b3e0c7849e4fa53b9f6d9fae0a1fc2
ALTU <skaltamash789@gmail.com>
2022-03-19 16:04:41 +0530

    list view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../models/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "ALTU";
+  // final int days = 30;
+  // final String name = "ALTU";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to Alpha Devlopers"),
-        ),
+      body: ListView.builder(
+        itemCount: CatalogModel.items.length,
+        itemBuilder: (context,index){
+          return ItemWidget(
+            item: CatalogModel.items[index],
+          );
+        },
       ),
       drawer: MyDrawer(),
     );
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}"),
+      ),
+    );
+  }
+}
